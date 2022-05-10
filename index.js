ef8d1d5943707e63b8033bb28ad0f9fd42a936bd
андрей пользователь <you@example.com>
2022-05-11 01:59:59 +0300

    init: press effect

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const keys = [
   "0",
   "-",
   "=",
-  "backspace",
-  "tab",
+  "Backspace",
+  "Tab",
   "q",
   "w",
   "e",
@@ -27,8 +27,8 @@ const keys = [
   "[",
   "]",
   "\\",
-  "del",
-  "capslock",
+  "Delete",
+  "CapsLock",
   "a",
   "s",
   "d",
@@ -40,8 +40,8 @@ const keys = [
   "l",
   ";",
   "'",
-  "enter",
-  "leftshift",
+  "Enter",
+  "Shift",
   "z",
   "x",
   "c",
@@ -52,19 +52,20 @@ const keys = [
   ",",
   ".",
   "/",
-  "up",
-  "rightshift",
-  "leftctrl",
-  "win",
-  "leftalt",
-  "space",
-  "rightalt",
-  "left",
-  "down",
-  "right",
-  "rightctrl",
+  "ArrowUp",
+  "Shift",
+  "Control",
+  "Meta",
+  "Alt",
+  "Space",
+  "Alt",
+  "ArrowLeft",
+  "ArrowDown",
+  "ArrowRight",
+  "Control",
 ];
 
+// let body = document.body;
 let p = document.createElement("p");
 p.classList.add("title");
 p.innerHTML = "RSS Виртуальная клавиатура";
@@ -72,14 +73,17 @@ document.body.appendChild(p);
 
 let textarea = document.createElement("textarea");
 textarea.classList.add("textarea");
-
+// textarea.setAttribute('')
 document.body.appendChild(textarea);
 
 let keyboard = document.createElement("div");
 keyboard.classList.add("keyboard");
 document.body.appendChild(keyboard);
 createKey();
+let keyButton = document.querySelectorAll("button");
 
+// console.log("body :>> ", body);
+// console.log("keyboard :>> ", keyboard);
 function createKey() {
   keys.forEach((elem) => {
     let key = document.createElement("button");
@@ -87,63 +91,167 @@ function createKey() {
     key.classList.add("key");
 
     switch (elem) {
-      case "backspace":
+      case "Backspace":
         key.classList.add("backspace");
-        key.innerHTML = "Backspace";
         break;
-      case "tab":
+      case "Tab":
         key.classList.add("tab");
-        key.innerHTML = "Tab";
         break;
-      case "del":
+      case "Delete":
         key.innerHTML = "Del";
         break;
-      case "capslock":
+      case "CapsLock":
         key.classList.add("capslock");
-        key.innerHTML = "CapsLock";
         break;
-      case "enter":
+      case "Enter":
         key.classList.add("enter");
-        key.innerHTML = "Enter";
         break;
-      case "leftshift":
-      case "rightshift":
+
+      case "Shift":
         key.classList.add("shift");
-        key.innerHTML = "Shift";
         break;
-      case "leftctrl":
-      case "rightctrl":
+      case "Control":
         key.classList.add("ctrl");
         key.innerHTML = "Ctrl";
         break;
-      case "leftalt":
-      case "rightalt":
+      case "Alt":
         key.classList.add("alt");
-        key.innerHTML = "Alt";
         break;
-      case "win":
+      case "Meta":
         key.classList.add("win");
         key.innerHTML = "Win";
         break;
-      case "space":
+      case "Space":
         key.classList.add("space");
         key.innerHTML = "";
         break;
-      case "up":
+      case "ArrowUp":
         key.innerHTML = "↑";
         break;
-      case "right":
+      case "ArrowRight":
         key.innerHTML = "→";
         break;
-      case "left":
+      case "ArrowLeft":
         key.innerHTML = "←";
         break;
-      case "down":
+      case "ArrowDown":
         key.innerHTML = "↓";
         break;
     }
-    key.setAttribute("data-key", key);
+    // key.setAttribute("data-key", elem);
+    key.setAttribute("id", elem);
+    // key.addEventListener("click", (event) => mouseClick(event));
 
     keyboard.appendChild(key);
   });
 }
+
+// mouseClick(event) {
+// const content = event.currentTarget.dataset.key;
+// const textArea = this.elements.textarea;
+// let cursorPosition = textArea.selectionStart + 1;
+// let [value, start, end] = [
+//   (this.properties.isCapsLock) ? content.toUpperCase() : content,
+//   textArea.selectionStart,
+//   textArea.selectionEnd,
+// ];
+// switch (content) {
+//   case 'del':
+//     [value, end] = ['', textArea.selectionEnd + 1];
+//     break;
+//   case 'backspace':
+//     [value, start] = ['', textArea.selectionStart - 1];
+//     break;
+//   case 'enter':
+//     value = '\n';
+//     break;
+//   case 'tab':
+//     value = '\t';
+//     break;
+//   case 'backtick':
+//     value = '`';
+//     break;
+//   case 'spacebar':
+//     value = ' ';
+//     break;
+//   case 'capslock':
+//     event.currentTarget.classList.toggle('pressed');
+//     value = '';
+//     cursorPosition = textArea.selectionStart;
+//     this.capsLockOn();
+//     break;
+//   default:
+//     break;
+// }
+// if (start >= 0) textArea.setRangeText(value, start, end);
+// if (content === 'del' || content === 'backspace') {
+//   cursorPosition = textArea.selectionStart;
+// }
+
+// this.elements.textarea.focus();
+// this.elements.textarea.selectionStart = cursorPosition;
+// }
+
+// keyElement.setAttribute("data-key", key);
+
+// keyElement.addEventListener("click", (event) => this.mouseClick(event));
+
+// keyboardFragment.append(keyElement);
+
+document.onkeydown = function (event) {
+  //   keyButton.forEach((button) => {
+  //     button.classList.remove("active");
+  //   });
+  console.log("event.code :>> ", event.code);
+  console.log("event.code :>> ", event.key);
+  let activeKey = document.getElementById(event.key);
+  console.log("activeKey :>> ", activeKey);
+
+  if (activeKey === null) {
+    if (event.code == "Space") {
+      activeKey = document.getElementById(event.code);
+    }
+  }
+
+  if (activeKey !== null) {
+    activeKey.classList.add("active");
+  }
+};
+
+document.onkeyup = function (event) {
+  let activeKey = document.getElementById(event.key);
+  if (activeKey === null) {
+    if (event.code == "Space") {
+      activeKey = document.getElementById(event.code);
+    }
+  }
+  if (activeKey !== "null") {
+    activeKey.classList.remove("active");
+  }
+};
+
+// let keyButton = document.querySelectorAll("button");
+// console.log(keyButton);
+
+// keyButton.forEach((elem) => {
+//   //   elem.addEventListener("click", (event) => {
+//   //     //   this.mouseClick(event);
+//   //     console.log("event.code :>> ", event.code);
+//   //   });
+
+//   elem.onclick = function (event) {
+//     let code = this.getAttribute;
+//     console.log("event.code :>> ", elem.);
+//   };
+// });
+
+//     fcArray.forEach((fc) => {
+//       let cards = document.createElement("div");
+//       cards.classList.add("card");
+//       cards.dataset.fc = `${fc}`;
+//       let imgFront = document.createElement("img");
+//       cards.append(imgFront);
+//       imgFront.src = `./img/png/${fc}.png`;
+//       imgFront.classList.add("front-face");
+//       let imgBack = document.createElement("img");
+//
